22a0593133186431eadf4f8de752e11cec1ae07e
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-10-08 16:22:35 +0100

    first commit

---
+++ src/sortJava/insertionSort.java
@@ -0,0 +1,20 @@
+package sortJava;
+
+public class insertionSort {
+
+    public static void performSort(int []data){
+
+        for (int currentIndex = 0; currentIndex < data.length; currentIndex++) {
+
+            int insertValue = data[currentIndex];
+            int moveIndex = currentIndex;
+
+            while (moveIndex > 0 && data[moveIndex-1] > insertValue){
+
+                data[moveIndex] = data[moveIndex-1];
+                moveIndex--;
+            }
+            data[moveIndex] = insertValue;
+        }
+    }
+}
