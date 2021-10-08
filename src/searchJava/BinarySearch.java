641604ef9e82ffe4dedf85043cb3fc10adbb6b78
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-10-09 00:57:25 +0100

    bibary libta

--- src/searchJava/BinarySearch.java
+++ src/searchJava/BinarySearch.java
@@ -1,2 +1,23 @@
-package searchJava;public class BinarySearch {
+package searchJava;
+
+public class BinarySearch {
+    public static int searchNumber(int [] numbers, int target) {
+
+        int high = numbers.length-1;
+        int low = 0;
+
+        while (low < high) {
+            int midPointNumber = ((high + low) / 2);
+
+            if (numbers[midPointNumber] == target)
+                return midPointNumber;
+
+            else
+                if (numbers[midPointNumber] < target)
+                    low = midPointNumber -1;
+
+                else high = midPointNumber + 1;
+        }
+        return  -1;
+    }
 }
