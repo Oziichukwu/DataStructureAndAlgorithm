870e2a1903d833d45458821d5e0e2d4b4127b93a
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-10-09 19:33:32 +0100

    linear search test

--- test/searchJava/LinearSearchTest.java
+++ test/searchJava/LinearSearchTest.java
@@ -6,9 +6,7 @@ public class LinearSearchTest {
 
     @Test
     public void test_CanSearchForTheIndexLinearSearch(){
-
         int [] numbers = {2,3,4,5,6,7,8,9,19,12};
-
         int target = 8;
         assertEquals(6,LinearSearch.searchNumber(numbers,target));
     }
