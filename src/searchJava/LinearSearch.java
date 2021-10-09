7a8d99f58540a3ff5d54182947922dc4e06da163
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-10-09 01:00:18 +0100

    new

--- src/searchJava/LinearSearch.java
+++ src/searchJava/LinearSearch.java
@@ -1,2 +1,15 @@
-package searchJava;public class LinearSearch {
+package searchJava;
+
+public class LinearSearch {
+
+
+    public static int searchNumber(int [] numbers, int target) {
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i].compareTo())
+                return i;
+        }
+
+        return -1;
+    }
 }
--- test/searchJava/LinearSearchTest.java
+++ test/searchJava/LinearSearchTest.java
@@ -1,2 +1,16 @@
-package searchJava;public class LinearSearchTest {
+package searchJava;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+public class LinearSearchTest {
+
+    @Test
+    public void test_CanSearchForTheIndexLinearSearch(){
+
+        int [] numbers = {2,3,4,5,6,7,8,9,19,12};
+
+        int target = 8;
+        assertEquals(6,LinearSearch.searchNumber(numbers,target));
+    }
+
 }
