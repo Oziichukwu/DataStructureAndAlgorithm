bb1f189fce65f4611cb1ee2ed69b2680cc4749a7
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-10-09 19:34:06 +0100

    binary search

--- test/searchJava/BinarySearchTest.java
+++ test/searchJava/BinarySearchTest.java
@@ -1,2 +1,13 @@
-package searchJava;public class BinarySearchTest {
+package searchJava;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BinarySearchTest {
+    @Test
+    public void test_CanFindIndexUsingBinarySearch() {
+        int [] numbers = {2,3,4,5,6,7,8,9,12,13};
+        int target = 3;
+        assertEquals(1, BinarySearch.searchNumber(numbers,target));
+    }
 }
