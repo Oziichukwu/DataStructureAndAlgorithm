7e88824acd0da03372f160efa9a18fbd9e75bf8f
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-10-09 19:36:42 +0100

    binary search

--- src/searchJava/BinarySearch.java
+++ src/searchJava/BinarySearch.java
@@ -6,17 +6,17 @@ public class BinarySearch {
         int high = numbers.length-1;
         int low = 0;
 
-        while (low < high) {
-            int midPointNumber = ((high + low) / 2);
+        while (low <= high) {
+            int midPointNumber =(high + low) /2; //low + ((high - low) / 2);
 
             if (numbers[midPointNumber] == target)
                 return midPointNumber;
 
             else
                 if (numbers[midPointNumber] < target)
-                    low = midPointNumber -1;
+                    low = midPointNumber + 1;
 
-                else high = midPointNumber + 1;
+                else high = midPointNumber - 1;
         }
         return  -1;
     }
