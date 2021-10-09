abb74b8e6c1cd89f00a6740b7e40ce06fa07d801
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-10-09 19:36:14 +0100

    linear search

--- src/searchJava/LinearSearch.java
+++ src/searchJava/LinearSearch.java
@@ -6,10 +6,9 @@ public class LinearSearch {
     public static int searchNumber(int [] numbers, int target) {
 
         for (int i = 0; i < numbers.length; i++) {
-            if (numbers[i].compareTo())
+            if (numbers[i] == target)
                 return i;
         }
-
         return -1;
     }
 }
