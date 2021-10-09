7a0964005a20dd1fbde794a93cab8b4e6d0bbd51
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-10-09 19:35:20 +0100

    added main

--- src/sortJava/SortMain.java
+++ src/sortJava/SortMain.java
@@ -1,2 +1,40 @@
-package sortJava;public class SortMain {
+package sortJava;
+
+import java.security.SecureRandom;
+import java.util.Arrays;
+
+public class SortMain {
+
+    public static void main(String[] args) {
+
+        int [] numbersToSort = new int[20];
+
+        SecureRandom mySecureRandom = new SecureRandom();
+
+        for (int i = 0; i < numbersToSort.length; i++) {
+
+            numbersToSort[i] = 10 + mySecureRandom.nextInt(90);
+
+        }
+        System.out.println("These numbers are unsorted" + Arrays.toString(numbersToSort));
+
+        insertionSort.performSort(numbersToSort);
+
+        System.out.println("These numbers are now sorted" + Arrays.toString(numbersToSort));
+
+        System.out.println();
+        System.out.println("This is a merge sort application");
+
+        for (int i = 0; i < numbersToSort.length; i++) {
+
+            numbersToSort[i] = 10 + mySecureRandom.nextInt(90);
+
+        }
+        System.out.println("unsorted numbers" + Arrays.toString(numbersToSort));
+
+        MergeSort.mergeSort(numbersToSort,0, numbersToSort.length-1);
+        System.out.println("sorted elements" + Arrays.toString(numbersToSort));
+
+    }
+
 }
