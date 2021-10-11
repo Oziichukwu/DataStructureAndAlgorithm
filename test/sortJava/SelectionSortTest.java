43ed71a57b120262db99ed6b424a50de7c14dc3a
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-10-11 06:17:28 +0100

    selection sort

---
+++ test/sortJava/SelectionSortTest.java
@@ -0,0 +1,13 @@
+package sortJava;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+public class SelectionSortTest {
+
+    @Test
+    public void testThatSelectionSortCanBePerformed(){
+
+        int [] array = {2,9,5,14,8,15,1,46};
+        assertArrayEquals(new int[]{1,2,5,8,9,14,15,46}, SelectionSort.selectionSort2(array));
+    }
+}
