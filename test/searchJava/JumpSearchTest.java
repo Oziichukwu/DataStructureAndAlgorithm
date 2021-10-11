7404b5cbf25b30ef0d620a2137deb1396b89ad61
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-10-11 06:20:30 +0100

    jump sort

--- test/searchJava/JumpSearchTest.java
+++ test/searchJava/JumpSearchTest.java
@@ -1,2 +1,15 @@
-package searchJava;public class JumpSearchTest {
+package searchJava;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class JumpSearchTest {
+
+    @Test
+    public void test_CanFindElementAtIndex(){
+
+        int [] numbers = {2,3,4,5,6,7,8,9};
+        int target = 3;
+        assertEquals(1, JumpSearch.performJumpSearch(numbers,target));
+    }
 }
