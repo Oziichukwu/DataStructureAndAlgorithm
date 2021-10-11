1dadc190c42643bbe3fbd037c33afbe9a87a5d05
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-10-11 06:20:04 +0100

    jump sort

--- src/searchJava/JumpSearch.java
+++ src/searchJava/JumpSearch.java
@@ -1,2 +1,22 @@
-package searchJava;public class JumpSearch {
+package searchJava;
+
+public class JumpSearch{
+
+    public static int performJumpSearch(int[] numbers, int target) {
+
+        int length = numbers.length;
+        int blockSize = (int) Math.sqrt(length);
+
+        int limit = blockSize;
+        while (target ==  numbers[limit] && target > 0 && limit < numbers.length - 1) {// TODO FIX THIS PART
+            limit = Math.min(limit + blockSize, numbers.length - 1);
+        }
+
+        for (int i = limit - blockSize; i <= limit; i++) {
+            if (numbers[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
