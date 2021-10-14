626cb184049796351e029dedefee8fdbe727c4f9
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-10-14 01:28:16 +0100

    reverse strings

--- src/strings/ReverseStrings.java
+++ src/strings/ReverseStrings.java
@@ -1,2 +1,24 @@
-package strings;public class ReverseStrings {
+package strings;
+
+public class ReverseStrings {
+
+    public static String performReverse(String letters) {
+
+        return new StringBuilder(letters).reverse().toString();
+
+    }
+
+    public static String performReverse2(String letter) {
+
+        if (letter == null || letter.isEmpty()){
+            return letter;
+        }
+        char [] character = letter.toCharArray();
+        for (int i = 0, j = letter.length()-1; i < j; i++, j--) {
+            char temp = character[i];
+            character[i] = character[j];
+            character[j] = temp;
+        }
+        return new String(character);
+    }
 }
--- src/strings/SameCharacter.java
+++ src/strings/SameCharacter.java
@@ -1,2 +1,15 @@
-package strings;public class SameCharacter {
+package strings;
+
+public class SameCharacter {
+
+    public static boolean checkIfCharactersAreSimilar(String letters) {
+
+        for (int i = 1; i < letters.length(); i++) {
+
+            if (letters.charAt(i) != letters.charAt(0)){
+                return false;
+            }
+        }
+        return true;
+    }
 }
