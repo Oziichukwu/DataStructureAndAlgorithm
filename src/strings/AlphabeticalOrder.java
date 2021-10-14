769890b5de9dcd7a6bd1511ac784a6c11abec3e8
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-10-14 01:28:52 +0100

    alphabetical order

--- src/strings/AlphabeticalOrder.java
+++ src/strings/AlphabeticalOrder.java
@@ -1,2 +1,16 @@
-package strings;public class AlphabeticalOrder {
+package strings;
+
+public class AlphabeticalOrder {
+
+    public static boolean checkIfItsInAlphabeticalOrder(String letters) {
+
+        letters = letters.toLowerCase();
+        for (int i = 0; i < letters.length() - 1; i++) {
+            if (!Character.isLetter(letters.charAt(i)) || !(letters.charAt(i) <= letters.charAt(i + 1))) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
+
