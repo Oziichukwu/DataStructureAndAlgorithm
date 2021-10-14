0060847060c4849cc0991a464a295fc3dc5ce1a2
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-10-14 01:27:02 +0100

    reverse

--- test/strings/ReverseStringsTest.java
+++ test/strings/ReverseStringsTest.java
@@ -1,11 +1,24 @@
 package strings;
 
 import org.junit.jupiter.api.Test;
-public class ReverseStrings {
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ReverseStringsTest {
 
     @Test
     public void stringsCanBeReversedTest(){
-        String letters = 
+
+        String letters = "abc123";
+        assertEquals("321cba", ReverseStrings.performReverse(letters));
+
+    }
+
+    @Test
+    public void stringsCanBeReversedTest2(){
+
+        String letter = "321cba";
+        assertEquals("abc123", ReverseStrings.performReverse2(letter));
 
     }
 }
