5fa699301d41d92a8a29d5bd0a19246fbf69a419
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-10-15 01:19:20 +0100

    anagram

--- src/strings/CheckAnagram.java
+++ src/strings/CheckAnagram.java
@@ -1,2 +1,19 @@
-package strings;public class CheckAnagram {
+package strings;
+
+import java.util.Arrays;
+
+public class CheckAnagram {
+
+    public static boolean checkIfTheyAreAnagram(String letter1, String letter2){
+
+        letter1 = letter1.toLowerCase();
+        letter2 = letter2.toLowerCase();
+
+        char [] character1 = letter1.toCharArray();
+        char[] character2 = letter2.toCharArray();
+
+        Arrays.sort(character1);
+        Arrays.sort(character2);
+        return new String(character1).equals(new String(character2));
+    }
 }
