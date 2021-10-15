79af0b7af6c10436016d5ae72abb4ad49fa4d4b8
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-10-15 01:18:19 +0100

    same character

--- src/strings/SameCharacter.java
+++ src/strings/SameCharacter.java
@@ -5,7 +5,6 @@ public class SameCharacter {
     public static boolean checkIfCharactersAreSimilar(String letters) {
 
         for (int i = 1; i < letters.length(); i++) {
-
             if (letters.charAt(i) != letters.charAt(0)){
                 return false;
             }
