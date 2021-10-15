168c362a30e10beefc2b89bf92cd6693ec26ca1a
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-10-15 01:59:16 +0100

    convertion

--- src/strings/ConvertionToUpperCase.java
+++ src/strings/ConvertionToUpperCase.java
@@ -1,2 +1,17 @@
-package strings;public class ConvertionToUpperCase {
+package strings;
+
+public class ConvertionToUpperCase {
+
+    public static String convert(String letters){
+
+        char [] characters = letters.toCharArray();
+
+        for (int i = 0; i < characters.length; i++) {
+
+            if (Character.isLetter(characters[i]) && Character.isLowerCase(characters[i])){
+                characters[i] = Character.toUpperCase(characters[i]);
+            }
+        }
+        return new String(characters);
+    }
 }
