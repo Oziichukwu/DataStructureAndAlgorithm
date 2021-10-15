629ce13dcefb03ba18f0e0ffe14b6432e8d73528
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-10-15 02:50:05 +0100

    g

--- src/arrayTasks/SecondLargest.java
+++ src/arrayTasks/SecondLargest.java
@@ -1,2 +1,27 @@
-package arrayTasks;public class SecondLargest {
-}
+package arrayTasks;
+
+public class SecondLargest {
+
+    public static int secondLargest(int [] numbers) {
+        int temp;
+        for (int i = 0; i < numbers.length; i++)
+        {
+            for (int j = i + 1; j < numbers.length; j++)
+            {
+                if (numbers[i] > numbers[j])
+                {
+                    temp = numbers[i];
+                    numbers[i] = numbers[j];
+                    numbers[j] = temp;
+                }
+            }
+        }
+        return numbers[numbers.length-2];
+    }
+
+    public static void main(String[] args) {
+        int [] numbers = {5,6,2,7,8,11,21};
+        System.out.println(secondLargest(numbers));
+    }
+    }
+
--- src/strings/AlphabeticalOrder.java
+++ src/strings/AlphabeticalOrder.java
@@ -12,5 +12,6 @@ public class AlphabeticalOrder {
         }
         return true;
     }
+
 }
 
