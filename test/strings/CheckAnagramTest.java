412f07aed67bb214409c56b14044c1d1e44e1762
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-10-15 01:17:49 +0100

    anagram

--- test/strings/CheckAnagramTest.java
+++ test/strings/CheckAnagramTest.java
@@ -1,2 +1,14 @@
-package strings;public class CheckAnagramTest {
+package strings;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+public class CheckAnagramTest {
+
+    @Test
+    public void checkThatTwoStringsAreAnagram(){
+
+        String letter1 = "is this how you Listen";
+        String letter2 = "this is how you listen";
+        assertTrue(CheckAnagram.checkIfTheyAreAnagram(letter1,letter2));
+    }
 }
