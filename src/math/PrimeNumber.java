9940c20fef3f4c1ae17d2bc118610cead417c54b
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-10-28 02:56:50 +0100

    prime numbrs

--- src/math/PrimeNumber.java
+++ src/math/PrimeNumber.java
@@ -1,2 +1,20 @@
-package math;public class PrimeNumber {
+package math;
+
+public class PrimeNumber {
+
+    public static boolean isPrimeNumber(int number) {
+        if (number == 2){
+            return true;
+        }else
+            if (number < 2 || number % 2 == 0){
+                return false;
+            }
+        for (int i = 3, limit = (int)Math.sqrt(number); i < limit; i+=2) {
+
+            if (number % i == 0){
+                return false;
+            }
+        }
+        return  true;
+    }
 }
