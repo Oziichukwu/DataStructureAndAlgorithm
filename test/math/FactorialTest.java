ba3d158c60311a65ebacb22ca1c3dc7033c0abdf
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-10-28 02:55:57 +0100

    factorial

--- test/math/FactorialTest.java
+++ test/math/FactorialTest.java
@@ -1,4 +1,18 @@
 package math;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 public class FactorialTest {
+
+    @Test
+    public void performFactorial(){
+
+        int number = 5;
+
+        int expected = 120;
+
+        int actual = Factorial.performFactorial(number);
+
+        assertEquals(expected, actual);
+    }
 }
