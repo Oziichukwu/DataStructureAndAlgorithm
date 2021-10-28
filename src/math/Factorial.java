a93aa2dc4b8403544e001da154bcb862c8bf8d3c
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-10-28 03:29:41 +0100

    factorial

--- src/math/Factorial.java
+++ src/math/Factorial.java
@@ -1,2 +1,19 @@
-package math;public class Factorial {
+package math;
+
+public class Factorial {
+
+    public static int performFactorial(int number) {
+
+        if (number < 0){
+            throw new IllegalArgumentException("number is negative");
+        }
+
+        int factorial = 1;
+        for (int i = 1; i <= number; factorial*=i, ++i) {
+            ;
+        }
+
+
+        return factorial;
+    }
 }
