0b16086a12c5b3ae94985a1e957decc96334a00f
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-11-11 02:39:16 +0100

    stack implementation

--- src/queueImpl/Queue.java
+++ src/queueImpl/Queue.java
@@ -1,2 +1,69 @@
-package queueImpl;public class Queuea {
+package queueImpl;
+
+public class Queue {
+
+    private final int maximumSize;
+    private final int[]queueArray;
+    private int frontEnd;
+    private int rearEnd;
+    private int count;
+    public Queue(int size) {
+        this.maximumSize = size;
+        queueArray = new int[size];
+        frontEnd = -1;
+        rearEnd = -1;
+    }
+
+    public int getMaximumSize() {
+
+        return maximumSize;
+    }
+
+    public void enQueue(int itemAdded) throws Exception{
+        if (!isFull()){
+            if (frontEnd == -1)
+                frontEnd = 0;
+            rearEnd++;
+            queueArray[rearEnd] = itemAdded;
+            count++;
+        }else
+        {
+            throw new Exception("Queue is currently full and cannot take more items");
+        }
+    }
+
+    public boolean isFull() {
+        return frontEnd == 0 && rearEnd == maximumSize - 1;
+    }
+
+    public int size() {
+        return  count;
+    }
+
+
+    public int deQueue() throws Exception{
+
+        if (!isEmpty()){
+            int itemToRemove = queueArray[frontEnd];
+            if (frontEnd >= rearEnd){
+                frontEnd = -1;
+                rearEnd = -1;
+            }else {
+                frontEnd++;
+                count--;
+            }
+            return itemToRemove;
+        }else {
+            throw new Exception("Queue is currently empty and cannot remove any item");
+        }
+    }
+
+    public boolean isEmpty() {
+        return frontEnd == -1 && rearEnd == -1;
+    }
+
+    public int peek() {
+
+        return queueArray[frontEnd];
+    }
 }
--- src/stackImpl/Stack.java
+++ src/stackImpl/Stack.java
@@ -1,2 +1,93 @@
-package stackImpl;public class Stacka {
+package stackImpl;
+
+public class Stack {
+
+    private final int maximumSize;
+    private final int[] stackArray;
+    private int topOfStack;
+
+    public Stack(int size) {
+        this.maximumSize = size;
+        stackArray = new int[size];
+        topOfStack = - 1;
+    }
+
+    public int getMaximumSize() {
+        return maximumSize;
+    }
+
+    public void push(int valueAdded) throws Exception{
+        if (!isFull()){
+            stackArray[topOfStack + 1] = valueAdded;
+            topOfStack++;
+        }else
+        {
+            throw new Exception("Stack is currently full and cannot push more items");
+        }
+    }
+
+    public boolean isFull() {
+        int maximumValueInStack = maximumSize - 1;
+        if (topOfStack == maximumValueInStack){
+            return true;
+        }
+        return false;
+    }
+
+    public int size() {
+
+        return topOfStack + 1;
+    }
+
+    public int pop() throws Exception{
+        if (!isEmpty()){
+            int valueToRemove = stackArray[topOfStack];
+            stackArray[topOfStack] = 0;
+            topOfStack--;
+            return valueToRemove;
+        }else{
+            throw  new Exception("Stack is currently empty and cannot remove more items");
+        }
+    }
+
+    public boolean isEmpty() {
+        if (topOfStack == -1){
+            return true;
+        }
+        return false;
+    }
+
+    public int search(int itemToSearch) {
+
+        boolean itemFound = false;
+
+        int [] reversedStackArray = reverseArray(stackArray, stackArray.length);
+        int index = -1;
+        for (int i = 0; i < reversedStackArray.length; i++) {
+            if (reversedStackArray[i] == itemToSearch){
+                index = i;
+                itemFound = true;
+            }
+        }
+        if (itemFound){
+            return index + 1;
+        }else {
+            return index;
+        }
+    }
+
+    private int[] reverseArray(int[] array, int arrayLength) {
+
+        int [] reversedArray = new int[arrayLength];
+        int limit = array.length;
+        for (int i = 0; i < arrayLength; i++) {
+            reversedArray[limit-1] = array[i];
+            limit -= 1;
+        }
+        return reversedArray;
+    }
+
+    public int peek() {
+        return stackArray[topOfStack];
+    }
 }
